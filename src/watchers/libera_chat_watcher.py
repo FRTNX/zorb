c2098b088c51ebbf5a214d225225a7b7b0fd8a6d
FRTNX <frtnx@protonmail.com>
2024-06-27 19:44:27 +0200

    Updated config

--- src/watchers/libera_chat_watcher.py
+++ src/watchers/libera_chat_watcher.py
@@ -110,7 +110,7 @@ class FileHandler:
             log_count = len(existing_logs)
             if log_count > 1000:
                 # note: adjust according to intake requirements
-                start_index = log_count - 111
+                start_index = log_count - 222
                 existing_logs = existing_logs[start_index:]
 
             for log in existing_logs:
